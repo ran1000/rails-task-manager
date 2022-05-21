9ccab25c6bf465234365571189230ded351a205d
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-05-21 21:40:27 +0200

    add action for creating a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,25 @@
 class TasksController < ApplicationController
-  def list
+  def index
     @tasks = Task.all
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_parameters)
+    @task.save
+    redirect_to root_path
+  end
+
+  private
+
+  def task_parameters
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "index", to: "tasks#list"
+  root "tasks#index"
+  get "index", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: 'new'
   get "tasks/:id", to: "tasks#show", as: 'task'
+  post "tasks", to: "tasks#create"
 end
