e454ebc3891376efa182acf4a76bc5043e33978d
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-05-22 16:01:40 +0200

    add bootstrap classes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -17,7 +19,27 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_parameters)
+    redirect_to root_path
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_parameters
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "tasks#index"
-  get "index", to: "tasks#index"
+  # Read all
+  get "tasks", to: "tasks#index"
+  # Create
   get "tasks/new", to: "tasks#new", as: 'new'
-  get "tasks/:id", to: "tasks#show", as: 'task'
   post "tasks", to: "tasks#create"
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: 'task'
+  # Update
+  get "tasks/:id/edit", to: "tasks#edit", as: 'edit'
+  patch "tasks/:id", to: "tasks#update"
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
